8cd9bdea4567f300b18ae22a940f9433bba6bbeb
bill-code-12 <bernardbrighton045@gmail.com>
2026-08-25 09:46:18 +0300

    Add main App component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/globals.css';
+import Router from './routes';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-neutral-50">
+      <Router />
+    </div>
+  );
+}
+
+export default App;
